3e7ed9f7b37558d4ebcd40a8e9569e5cd549e16b
Dan Keegan <dan_keegan@Dan-Keegans-MacBook-Air.local>
2011-04-17 10:46:03 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DMDB::Application.config.secret_token = '54ac78bd5d797ec1be245b01f554dc113e549a52dc1ac869413f5bdaad426851a357491d27eeb79b33e9d398ede5d4c4406d088ee5a6609ce28360c4c901da61'
